b9b10b6832dc2d6b1451b0be01fd5781282f765e
Rubén <151085484+RubenMaxy@users.noreply.github.com>
2025-01-10 12:58:13 +0100

    .

--- 2Evaluacion/Trivial/app/src/main/java/com/example/trivial/ui/state/TrivialUiState.kt
+++ 2Evaluacion/Trivial/app/src/main/java/com/example/trivial/ui/state/TrivialUiState.kt
@@ -5,6 +5,7 @@ import com.example.trivial.data.QuestionData
 
 data class TrivialUiState(
     val numberQuestions: Int=5,
+    val actualQuestion: Int=0,
     val record:Int=0,
     val listQuestions: List<QuestionData> = emptyList(),
     val correctPercent: Int =0,
