24c7703aea5624fa61df5bac6953cea59923944a
Rubén <151085484+RubenMaxy@users.noreply.github.com>
2025-01-11 16:59:22 +0100

    .

--- Trivial/app/src/main/java/com/example/trivial/model/Questions.kt
+++
@@ -1,9 +0,0 @@
-package com.example.trivial.model
-
-import com.example.trivial.data.QuestionData
-import com.example.trivial.data.videoGameQuestions
-
-data class Questions(val videoGameQuestions: QuestionData) {}
-
-fun getRandomQuestions(numberOfQuestions: Int=5): List<QuestionData> {
-    return videoGameQuestions.shuffled().take(numberOfQuestions) }
--- Trivial/app/src/main/java/com/example/trivial/ui/navigation/AppScreens.kt
+++
@@ -1,7 +0,0 @@
-package com.example.trivial.ui.navigation
-
-enum class AppScreens {
-    HOME,
-    GAME,
-    ENDGAME
-}
