c4c02f700eb2da64a869dd3c5771f80d76f250af
Rubén <151085484+RubenMaxy@users.noreply.github.com>
2025-01-14 18:03:21 +0100

    Problemas con navegacion
    No navega de forma adecuada, algo le pasa al estado

--- 2Evaluacion/Trivial/app/src/main/java/com/example/trivial/ui/screens/EndGame.kt
+++ 2Evaluacion/Trivial/app/src/main/java/com/example/trivial/ui/screens/EndGame.kt
@@ -1,9 +1,12 @@
 package com.example.trivial.ui.screens
 
 import android.annotation.SuppressLint
+import android.widget.Space
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -35,9 +38,19 @@ fun EndGame(
         modifier = Modifier
     ) {
         Column (modifier = Modifier
-            .padding(top = 100.dp)){
+            .padding(top = 110.dp)){
             Text(text = "Has terminado el juego")
+            Spacer(modifier = Modifier.padding(2.dp))
             Text(text = "Su puntuacion es: ${trivialState.correctPercent}%")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Button(
+                onClick = {
+                    navigateToHome()
+                },
+                modifier = Modifier
+            ) {
+                Text(text = "Volver al inicio")
+            }
         }
     }
 }
